3c73c622a153b37be97feda1460e13eb3b7e28e6
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-11 16:32:05 +0500

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,21 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
+      int k = c - 2;
 
+        for (int i = 0; i < k; i++) {
+
+            for (int j = 0; j < k - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i ; j >= 0; j--) {
+                System.out.print(j + 1 );
+            }
+            for (int j = 1 ; j <= i ; j++) {
+                System.out.print(j + 1 );
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
