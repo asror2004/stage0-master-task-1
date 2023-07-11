f89e1db06cc0467396d0f9e46fbcacea924b57fd
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-11 13:10:59 +0500

    Update Animal.java

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,25 @@
 package com.epam.OOP;
 
 public class Animal {
+ String color;
+    int numberOfPaws;
+    boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public void   getDescription() {
+        String str ;
+        if (numberOfPaws == 1 ){
+            str = "paw" ;
+        }else{
+            str = "paws" ;
+        }
+
+        System.out.println("This animal is mostly " + color + ". " +
+                "It has " + numberOfPaws + " "+str+" and " + hasFur+" fur.");
+    }
 }
