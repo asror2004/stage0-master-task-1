c3ff1e3c8f8730a8aee58cfb89587bfb67db4f75
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-11 13:54:03 +0500

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,8 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        StringBuilder reverseNumber = new StringBuilder(String.valueOf(number));
+        System.out.println(reverseNumber.reverse());
     }
 
 }
