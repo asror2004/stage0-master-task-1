0986981b4faaa4fb7d22abf19276faa5030b03b7
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-11 13:50:13 +0500

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,14 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
+        if (year < 0 || month < 0) {
+            System.out.println("invalid date");
+        }
 
+        LocalDate localDate = LocalDate.of(year, month, 1);
+        System.out.println("Leap year");
+
+        System.out.println("localDate.getDayOfMonth() = " + localDate.getDayOfMonth());
     }
 
 }
