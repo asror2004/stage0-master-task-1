49f92464bc8f31e555267c90da1b36970fd70027
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-12 18:20:55 +0500

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -5,10 +5,4 @@ public class Bird {
     public Bird() {
         super("blue", 2, false);
     }
-
-    @Override
-    public void getDescription() {
-        System.out.println(" This animal is mostly blue. " +
-                "It has 2 paws and no fur. Moreover, it has 2 wings and can fly.") ;
-    }
 }
