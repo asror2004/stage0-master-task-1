ef7dc5225cf899d77d57b431ec3d576bf110f8f6
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-12 22:41:24 +0500

    Update MultiplicationTable.java

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -3,7 +3,7 @@ package com.epam.loops;
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
-        if (numberTableToPrint <= 0){
+        if (numberTableToPrint < 0){
             System.out.println("Please enter positive number");
         }else {
             for (int i = 1 ; i <= 9 ; i++){
