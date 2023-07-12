a69381c08d73f9c2d7967368b95987ca20648338
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-12 21:51:07 +0500

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,11 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
-         int sum = 0 ;
-        while (number != 0){
-            sum += number % 10 ;
-            number = number / 10 ;
-        }
+        int a1 = number % 10;
+        int a2 = number / 10 % 10 ;
+        int a3 = number /100 % 10 ;
+        int a4 = number /1000 / 10 ;
+        int sum = a1 + a2 + a3 + a4;
         System.out.println(sum);
     }
 
