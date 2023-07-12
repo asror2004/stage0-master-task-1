deb17f4ea4e5536cba4f63642b8c90b1eae522f1
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-12 22:56:49 +0500

    Update SeasonDeterminer.java

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -9,7 +9,7 @@ public class SeasonDeterminer {
             case 3, 4, 5 -> System.out.println("Spring");
             case 6, 7, 8 -> System.out.println("Summer");
             case 9, 10, 11 -> System.out.println("Autumn");
-            default :  System.out.println("Wrong month number");
+            default ->  System.out.println("Wrong month number");
         }
 
     }
