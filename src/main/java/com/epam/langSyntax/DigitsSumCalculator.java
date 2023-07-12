fac15ee247f7fe5d7d59fde581d30c3d030d0c21
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-12 23:47:46 +0500

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -6,7 +6,7 @@ public class DigitsSumCalculator {
         int a1 = number % 10;
         int a2 = number / 10 % 10 ;
         int a3 = number /100 % 10 ;
-        int a4 = number /1000 / 10 ;
+        int a4 = number /1000 % 10 ;
         int sum = a1 + a2 + a3 + a4;
         System.out.println(sum);
     }
