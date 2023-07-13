46f9cc0b2e163fe153b7a9a1c9203f01395f5b4a
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-13 09:36:25 +0500

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,16 +3,34 @@ import java.util.Calendar;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-        if (year < 0 || month < 0 || month > 12) {
+        if (year < 0 || month < 0) {
             System.out.println("invalid date");
         }
-        Calendar calendar = Calendar.getInstance();
-      
-        calendar.set(year, month, 1);
-        int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int number = 0;
+        LocalDate localDate = LocalDate.of(year, month, 1);
+        boolean leapYear = localDate.isLeapYear();
 
-        System.out.println(maxDay);
+        switch (month){
+            case 1 :
+            case 3 :
+            case 5 :
+            case 7 :
+            case 8 :
+            case 10 :
+            case 12 :
+                number = 31 ;
+                break;
+            case 4 :
+            case 6 :
+            case 9 :
+            case 11 :
+                number = 30;
+                break;
+            case 2 : number = leapYear ? 29 : 28 ;
 
+                System.out.println(number);
+        }
+        }
     }
 
 }
