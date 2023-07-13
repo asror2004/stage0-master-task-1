747e3d1109ba001ad7b7c7ebf2fda7efbe4d2824
asror2004 <133206394+asror2004@users.noreply.github.com>
2023-07-13 09:42:18 +0500

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-        if (year < 0 || month < 0) {
+        if (year < 0 || month < 0 || month > 12) {
             System.out.println("invalid date");
         }
         int number = 0;
